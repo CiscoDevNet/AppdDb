9a72591aa759ad208240b9c4c4bddc747db33663
prathjan <prathjan@cisco.com>
2022-02-05 10:53:51 -0800

    quotes

--- scripts/installmysql.sh
+++ scripts/installmysql.sh
@@ -1,8 +1,8 @@
 echo $1
 sudo apt update
 apt-get install debconf
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password '$1''
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password '$1''
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password '$1'"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password '$1'"
 sudo apt-get -y install mysql-server
 sed 's/dbvmip/'$5'/g' /tmp/mysqld.cnf > /tmp/output.file
 sudo cp /tmp/output.file /etc/mysql/mysql.conf.d/mysqld.cnf
